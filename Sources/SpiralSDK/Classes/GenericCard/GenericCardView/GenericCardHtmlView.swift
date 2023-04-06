5a6086c8cf609d08b89f4c34b2712de8cd7e1971
Ron Soffer <rsoffer1@gmail.com>
2023-04-06 14:19:22 -0400

    additional UIKit imports

--- Sources/SpiralSDK/Classes/GenericCard/GenericCardView/GenericCardHtmlView.swift
+++ Sources/SpiralSDK/Classes/GenericCard/GenericCardView/GenericCardHtmlView.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import WebKit
+import UIKit
 
 class GenericCardHtmlView: GenericCardComponentView {
     
--- Sources/SpiralSDK/Classes/SpiralCommon/UIGestureRecognizer+Closure.swift
+++ Sources/SpiralSDK/Classes/SpiralCommon/UIGestureRecognizer+Closure.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 final class BindableTapGestureRecognizer: UITapGestureRecognizer {
     private var action: () -> Void
