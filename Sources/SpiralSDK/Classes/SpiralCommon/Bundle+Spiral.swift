603ffa13b1fe0e323b0d7289f9322679c81be9b4
Ron Soffer <rsoffer1@gmail.com>
2023-04-11 12:37:29 -0400

    try bundle.module

--- Sources/SpiralSDK/Classes/SpiralCommon/Bundle+Spiral.swift
+++ Sources/SpiralSDK/Classes/SpiralCommon/Bundle+Spiral.swift
@@ -14,6 +14,34 @@ extension Bundle {
             return bundle
         }
         
-        return Bundle(for: self)
+        return Bundle.module
     }
+}
+
+private class BundleFinder {}
+
+extension Foundation.Bundle {
+    /// Returns the resource bundle associated with the current Swift module.
+    static var module: Bundle = {
+        let bundleName = "SpiralSDK"
+
+        let candidates = [
+            // Bundle should be present here when the package is linked into an App.
+            Bundle.main.resourceURL,
+
+            // Bundle should be present here when the package is linked into a framework.
+            Bundle(for: BundleFinder.self).resourceURL,
+
+            // For command-line tools.
+            Bundle.main.bundleURL,
+        ]
+
+        for candidate in candidates {
+            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
+            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
+                return bundle
+            }
+        }
+        fatalError("unable to find bundle named SpiralSDK")
+    }()
 }
