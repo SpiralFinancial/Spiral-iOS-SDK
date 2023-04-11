b14814d5f911bc1feb79290ec0bdd677de18129a
Ron Soffer <rsoffer1@gmail.com>
2023-04-11 12:40:08 -0400

    explicit spmName func

--- Sources/SpiralSDK/Classes/SpiralCommon/Bundle+Spiral.swift
+++ Sources/SpiralSDK/Classes/SpiralCommon/Bundle+Spiral.swift
@@ -14,7 +14,7 @@ extension Bundle {
             return bundle
         }
         
-        return Bundle.module
+        return Bundle.spmModule
     }
 }
 
@@ -22,10 +22,21 @@ private class BundleFinder {}
 
 extension Foundation.Bundle {
     /// Returns the resource bundle associated with the current Swift module.
-    static var module: Bundle = {
-        let bundleName = "SpiralSDK"
+    static let spmModule: Bundle = {
+        let bundleName = "SpiralSDK_SpiralSDK"
 
-        let candidates = [
+        let overrides: [URL]
+        #if DEBUG
+        if let override = ProcessInfo.processInfo.environment["PACKAGE_RESOURCE_BUNDLE_URL"] {
+            overrides = [URL(fileURLWithPath: override)]
+        } else {
+            overrides = []
+        }
+        #else
+        overrides = []
+        #endif
+
+        let candidates = overrides + [
             // Bundle should be present here when the package is linked into an App.
             Bundle.main.resourceURL,
 
@@ -42,6 +53,6 @@ extension Foundation.Bundle {
                 return bundle
             }
         }
-        fatalError("unable to find bundle named SpiralSDK")
+        fatalError("unable to find bundle named SpiralSDK_SpiralSDK")
     }()
 }
